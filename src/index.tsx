f952f7ac538000f71d2934b5ffddbe133e90139d
Nicolas Toulemont <n.toulemont@gmail.com>
2020-04-26 21:22:40 +0200

    change prettier config to 100 printwidth and no semi

--- src/index.tsx
+++ src/index.tsx
@@ -1,190 +1,181 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 export interface Listener {
-  onSubmit?: Array<Function>;
-  onChange?: Array<Function>;
-  onBlur?: Array<Function>;
+  onSubmit?: Array<Function>
+  onChange?: Array<Function>
+  onBlur?: Array<Function>
 }
 
 export interface EventInfos {
-  name: string;
-  value: any;
+  name: string
+  value: any
 }
 
 export interface FieldOptions {
-  name: string;
-  listener?: Listener | undefined;
-  [key: string]: any;
+  name: string
+  listener?: Listener | undefined
+  [key: string]: any
 }
 
-export type FieldsRecord = Record<string | number, FieldOptions>;
+export type FieldsRecord = Record<string | number, FieldOptions>
 
 export function recordToArray(record: FieldsRecord): Array<FieldOptions> {
-  let arr: Array<FieldOptions> = [];
+  let arr: Array<FieldOptions> = []
   for (const key in record) {
-    arr.push({ ...record[key], name: key.toString() });
+    arr.push({ ...record[key], name: key.toString() })
   }
-  return arr;
+  return arr
 }
 
 export function arrayToRecord(array: Array<FieldOptions>) {
   return array.reduce((record: FieldsRecord, field: FieldOptions) => {
-    record[field.name] = { ...field };
-    return record;
-  }, {});
+    record[field.name] = { ...field }
+    return record
+  }, {})
 }
 
 export interface UseForm {
-  initialFields: Array<FieldOptions>;
-  state?: { [key: string]: any } | undefined;
+  initialFields: Array<FieldOptions>
+  state?: { [key: string]: any } | undefined
 }
 
 export function useForm({ state = {}, initialFields }: UseForm) {
-  const [record, setRecord] = useState<FieldsRecord>({});
-  const [fields, setFields] = useState<Array<FieldOptions>>(initialFields);
-  const [values, setValues] = useState<{ [key: string]: any }>(state);
-  const [errors, setErrors] = useState<{ [key: string]: any }>({});
-  const [hasSubmitted, setHasSubmitted] = useState<boolean>(false);
+  const [record, setRecord] = useState<FieldsRecord>({})
+  const [fields, setFields] = useState<Array<FieldOptions>>(initialFields)
+  const [values, setValues] = useState<{ [key: string]: any }>(state)
+  const [errors, setErrors] = useState<{ [key: string]: any }>({})
+  const [hasSubmitted, setHasSubmitted] = useState<boolean>(false)
 
   useEffect(() => {
-    setRecord(arrayToRecord(fields));
-  }, [fields]);
+    setRecord(arrayToRecord(fields))
+  }, [fields])
 
   // Shared
   function runFns(fns: Array<Function>, value: any): undefined | any {
     if (!fns || fns.length === 0) {
-      return undefined;
+      return undefined
     }
 
     // Return the first error its find
-    let err: undefined = undefined;
+    let err: undefined = undefined
     fns.forEach(fn => {
       if (typeof err === 'undefined' && typeof fn === 'function') {
-        err = fn(value);
+        err = fn(value)
       }
-    });
-    return err;
+    })
+    return err
   }
 
   function getFns(listener: Listener | undefined): Array<Function> {
-    let fns: Array<Function> = [];
+    let fns: Array<Function> = []
     if (listener?.onChange) {
-      fns = [...listener.onChange];
+      fns = [...listener.onChange]
     }
     if (listener?.onBlur) {
-      fns = [...fns, ...listener.onBlur];
+      fns = [...fns, ...listener.onBlur]
     }
     if (listener?.onSubmit) {
-      fns = [...fns, ...listener.onSubmit];
+      fns = [...fns, ...listener.onSubmit]
     }
-    return fns;
+    return fns
   }
 
   // On Change
   function onChange(infos: EventInfos) {
-    hasSubmitted && setHasSubmitted(false);
-    const { name, value } = infos;
+    hasSubmitted && setHasSubmitted(false)
+    const { name, value } = infos
     // Remove field error on change
-    if (errors[name]) setErrors({ ...errors, [name]: undefined });
+    if (errors[name]) setErrors({ ...errors, [name]: undefined })
 
     // Set values in state
     setValues({
       ...values,
       [name]: value,
-    });
+    })
 
     // If onChange listener, run it
     if (record[name].listener?.onChange) {
       setErrors({
         ...errors,
-        [name]: runFns(
-          record[name].listener?.onChange as Array<Function>,
-          value
-        ),
-      });
+        [name]: runFns(record[name].listener?.onChange as Array<Function>, value),
+      })
     }
   }
 
   function onBlur(infos: EventInfos) {
-    const { name, value } = infos;
+    const { name, value } = infos
 
     if (record[name].listener?.onBlur) {
       setErrors({
         ...errors,
         [name]: runFns(record[name].listener?.onBlur as Array<Function>, value),
-      });
+      })
     }
   }
 
   // On Submit listener
   function onSubmit() {
-    setHasSubmitted(true);
+    setHasSubmitted(true)
 
-    let count = 0;
-    let submitErrors: { [key: string]: any } = {};
+    let count = 0
+    let submitErrors: { [key: string]: any } = {}
 
     fields.forEach(field => {
       if (field.listener) {
         /* Reconcile onChange and onSubmit listeners: 
         onChange listener is also run on onSubmit */
-        const fns = getFns(field.listener);
+        const fns = getFns(field.listener)
 
         /* Build the submitErrors object in order to update the state later */
-        const errors = runFns(fns, values[field.name]);
-        submitErrors[field.name] = errors;
+        const errors = runFns(fns, values[field.name])
+        submitErrors[field.name] = errors
         // Increment errors to get the count
         if (errors) {
-          count++;
+          count++
         }
       }
-    });
+    })
 
     // Update errors state
-    setErrors(submitErrors);
+    setErrors(submitErrors)
 
     // Return the form valid status: true / false, and the form error count
-    return [count === 0, count];
+    return [count === 0, count]
   }
 
   function deleteVal(key: string) {
-    setValues(values => ({ ...values, [key]: undefined }));
+    setValues(values => ({ ...values, [key]: undefined }))
   }
 
   function deleteErr(key: string) {
     setErrors(errors => ({
       ...errors,
       [key]: undefined,
-    }));
+    }))
   }
 
-  const resetValues = () => setValues({});
-  const resetErrors = () => setErrors({});
+  const resetValues = () => setValues({})
+  const resetErrors = () => setErrors({})
 
   function resetForm() {
-    setErrors({});
-    setValues({});
+    setErrors({})
+    setValues({})
   }
 
   function removeFields(names: Array<string> | string) {
     if (typeof names === 'string') {
-      setFields(fields => fields.filter(field => field.name !== names));
+      setFields(fields => fields.filter(field => field.name !== names))
     } else {
-      setFields(fields =>
-        fields.filter(field => names.indexOf(field.name) === -1)
-      );
+      setFields(fields => fields.filter(field => names.indexOf(field.name) === -1))
     }
   }
 
   function addFields(newFields: Array<FieldOptions>, index: number) {
-    const updatedFields = [
-      ...fields.slice(0, index),
-      ...newFields,
-      ...fields.slice(index),
-    ];
-    setFields(updatedFields);
+    const updatedFields = [...fields.slice(0, index), ...newFields, ...fields.slice(index)]
+    setFields(updatedFields)
   }
 
-  const resetFields = () => setFields(initialFields);
+  const resetFields = () => setFields(initialFields)
 
   return {
     values,
@@ -205,5 +196,5 @@ export function useForm({ state = {}, initialFields }: UseForm) {
     removeFields,
     addFields,
     resetFields,
-  };
+  }
 }
--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import * as React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 
-import { useForm, FieldOptions } from '../src';
+import { useForm, FieldOptions } from '../src'
 
 function BasicComponent() {
   const required = (value: any) =>
-    typeof value === 'undefined' || value === '' ? 'Required' : undefined;
+    typeof value === 'undefined' || value === '' ? 'Required' : undefined
 
-  const hasAbc = (value: any) =>
-    value.includes('abc') ? 'Has abc' : undefined;
+  const hasAbc = (value: any) => (value.includes('abc') ? 'Has abc' : undefined)
 
   const initialFields: Array<FieldOptions> = [
     {
@@ -45,20 +44,14 @@ function BasicComponent() {
     {
       name: 'add',
       listener: {
-        onChange: [
-          () => addFields([{ name: 'new_field' }], initialFields.length),
-        ],
+        onChange: [() => addFields([{ name: 'new_field' }], initialFields.length)],
       },
     },
     {
       name: 'add-multiple',
       listener: {
         onChange: [
-          () =>
-            addFields(
-              [{ name: 'new_field_1' }, { name: 'new_field_2' }],
-              initialFields.length
-            ),
+          () => addFields([{ name: 'new_field_1' }, { name: 'new_field_2' }], initialFields.length),
         ],
       },
     },
@@ -77,7 +70,7 @@ function BasicComponent() {
     {
       name: 'first_name',
     },
-  ];
+  ]
 
   const {
     values,
@@ -96,10 +89,10 @@ function BasicComponent() {
     removeFields,
     addFields,
     resetFields,
-  } = useForm({ initialFields });
+  } = useForm({ initialFields })
 
   function handleSubmit() {
-    onSubmit();
+    onSubmit()
   }
 
   return (
@@ -110,15 +103,11 @@ function BasicComponent() {
             data-testid={field.name}
             name={field.name}
             value={values[field.name] || ''}
-            onChange={({ target: { name, value } }) =>
-              onChange({ name, value })
-            }
+            onChange={({ target: { name, value } }) => onChange({ name, value })}
             onBlur={({ target: { name, value } }) => onBlur({ name, value })}
           />
           {errors[field.name] && (
-            <div data-testid={field.name.concat('-error')}>
-              {errors[field.name]}
-            </div>
+            <div data-testid={field.name.concat('-error')}>{errors[field.name]}</div>
           )}
         </div>
       ))}
@@ -148,241 +137,237 @@ function BasicComponent() {
       </button>
       <button
         data-testid="set-val"
-        onClick={() =>
-          setValues({ email: 'settest@test.com', password: 'setpassword' })
-        }
+        onClick={() => setValues({ email: 'settest@test.com', password: 'setpassword' })}
       >
         set Values
       </button>
       <button
         data-testid="set-err"
-        onClick={() =>
-          setErrors({ email: 'email error', password: 'password error' })
-        }
+        onClick={() => setErrors({ email: 'email error', password: 'password error' })}
       >
         set Error
       </button>
     </div>
-  );
+  )
 }
 
 describe('Testing hook fns', () => {
   // Basic functionning
   it('Input and value', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    fireEvent.change(email, { target: { value: 'test@test.com' } });
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    fireEvent.change(email, { target: { value: 'test@test.com' } })
     // @ts-ignore
-    expect(email.value).toBe('test@test.com');
-  });
+    expect(email.value).toBe('test@test.com')
+  })
 
   // Validation
   it('onChange validation', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    fireEvent.change(email, { target: { value: 'abc' } });
-    const emailError = getByTestId('email-error');
-    expect(emailError).toBeVisible();
-  });
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    fireEvent.change(email, { target: { value: 'abc' } })
+    const emailError = getByTestId('email-error')
+    expect(emailError).toBeVisible()
+  })
 
   it('onBlur validation', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const password = getByTestId('password');
-    fireEvent.blur(password);
-    const passwordError = getByTestId('password-error');
-    expect(passwordError).toBeVisible();
-  });
+    const { getByTestId } = render(<BasicComponent />)
+    const password = getByTestId('password')
+    fireEvent.blur(password)
+    const passwordError = getByTestId('password-error')
+    expect(passwordError).toBeVisible()
+  })
 
   it('onSubmit validation', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    fireEvent.change(email, { target: { value: 'abc' } });
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    fireEvent.change(email, { target: { value: 'abc' } })
 
-    const submit = getByTestId('submit');
-    fireEvent.click(submit);
-    const emailError = getByTestId('email-error');
-    const addressError = getByTestId('address-error');
-    expect(emailError).toBeVisible();
-    expect(addressError).toBeVisible();
-  });
+    const submit = getByTestId('submit')
+    fireEvent.click(submit)
+    const emailError = getByTestId('email-error')
+    const addressError = getByTestId('address-error')
+    expect(emailError).toBeVisible()
+    expect(addressError).toBeVisible()
+  })
 
   // Deletes
   it('Delete item value', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    const password = getByTestId('password');
-    fireEvent.change(email, { target: { value: 'test@test.com' } });
-    fireEvent.change(password, { target: { value: 'password' } });
-    const deleteValBtn = getByTestId('delete-val');
-    fireEvent.click(deleteValBtn);
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    const password = getByTestId('password')
+    fireEvent.change(email, { target: { value: 'test@test.com' } })
+    fireEvent.change(password, { target: { value: 'password' } })
+    const deleteValBtn = getByTestId('delete-val')
+    fireEvent.click(deleteValBtn)
     // @ts-ignore
-    expect(email.value).toBe('');
+    expect(email.value).toBe('')
     // @ts-ignore
-    expect(password.value).toBe('');
-  });
+    expect(password.value).toBe('')
+  })
 
   it('Delete errors', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    const password = getByTestId('password');
-    fireEvent.change(email, { target: { value: 'abc' } });
-    fireEvent.blur(password);
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    const password = getByTestId('password')
+    fireEvent.change(email, { target: { value: 'abc' } })
+    fireEvent.blur(password)
 
-    const emailError = getByTestId('email-error');
-    const passwordError = getByTestId('password-error');
-    expect(emailError).toBeVisible();
-    expect(passwordError).toBeVisible();
+    const emailError = getByTestId('email-error')
+    const passwordError = getByTestId('password-error')
+    expect(emailError).toBeVisible()
+    expect(passwordError).toBeVisible()
 
-    const deleteErrBtn = getByTestId('delete-err');
-    fireEvent.click(deleteErrBtn);
-    expect(emailError).not.toBeInTheDocument();
-    expect(passwordError).not.toBeInTheDocument();
-  });
+    const deleteErrBtn = getByTestId('delete-err')
+    fireEvent.click(deleteErrBtn)
+    expect(emailError).not.toBeInTheDocument()
+    expect(passwordError).not.toBeInTheDocument()
+  })
 
   // Reset
   it('Reset all items value', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    const password = getByTestId('password');
-    const address = getByTestId('address');
-    fireEvent.change(email, { target: { value: 'test@test.com' } });
-    fireEvent.change(password, { target: { value: 'password' } });
-    fireEvent.change(address, { target: { value: 'address' } });
-    const deleteValBtn = getByTestId('reset-val');
-    fireEvent.click(deleteValBtn);
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    const password = getByTestId('password')
+    const address = getByTestId('address')
+    fireEvent.change(email, { target: { value: 'test@test.com' } })
+    fireEvent.change(password, { target: { value: 'password' } })
+    fireEvent.change(address, { target: { value: 'address' } })
+    const deleteValBtn = getByTestId('reset-val')
+    fireEvent.click(deleteValBtn)
     // @ts-ignore
-    expect(email.value).toBe('');
+    expect(email.value).toBe('')
     // @ts-ignore
-    expect(password.value).toBe('');
+    expect(password.value).toBe('')
     // @ts-ignore
-    expect(address.value).toBe('');
-  });
+    expect(address.value).toBe('')
+  })
 
   it('Reset all items errors', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    const password = getByTestId('password');
-    fireEvent.change(email, { target: { value: 'abc' } });
-    fireEvent.blur(password);
-    const submitBtn = getByTestId('submit');
-    fireEvent.click(submitBtn);
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    const password = getByTestId('password')
+    fireEvent.change(email, { target: { value: 'abc' } })
+    fireEvent.blur(password)
+    const submitBtn = getByTestId('submit')
+    fireEvent.click(submitBtn)
 
-    const emailError = getByTestId('email-error');
-    const passwordError = getByTestId('password-error');
-    const addressError = getByTestId('address-error');
-    expect(emailError).toBeVisible();
-    expect(passwordError).toBeVisible();
-    expect(addressError).toBeVisible();
+    const emailError = getByTestId('email-error')
+    const passwordError = getByTestId('password-error')
+    const addressError = getByTestId('address-error')
+    expect(emailError).toBeVisible()
+    expect(passwordError).toBeVisible()
+    expect(addressError).toBeVisible()
 
-    const resetErrs = getByTestId('reset-err');
-    fireEvent.click(resetErrs);
+    const resetErrs = getByTestId('reset-err')
+    fireEvent.click(resetErrs)
 
-    expect(emailError).not.toBeInTheDocument();
-    expect(passwordError).not.toBeInTheDocument();
-    expect(addressError).not.toBeInTheDocument();
-  });
+    expect(emailError).not.toBeInTheDocument()
+    expect(passwordError).not.toBeInTheDocument()
+    expect(addressError).not.toBeInTheDocument()
+  })
 
   it('Reset values and errors', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    const password = getByTestId('password');
-    fireEvent.change(email, { target: { value: 'abc' } });
-    fireEvent.blur(password);
-    const submitBtn = getByTestId('submit');
-    fireEvent.click(submitBtn);
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    const password = getByTestId('password')
+    fireEvent.change(email, { target: { value: 'abc' } })
+    fireEvent.blur(password)
+    const submitBtn = getByTestId('submit')
+    fireEvent.click(submitBtn)
 
-    const emailError = getByTestId('email-error');
-    const passwordError = getByTestId('password-error');
-    const addressError = getByTestId('address-error');
-    expect(emailError).toBeVisible();
-    expect(passwordError).toBeVisible();
-    expect(addressError).toBeVisible();
+    const emailError = getByTestId('email-error')
+    const passwordError = getByTestId('password-error')
+    const addressError = getByTestId('address-error')
+    expect(emailError).toBeVisible()
+    expect(passwordError).toBeVisible()
+    expect(addressError).toBeVisible()
 
-    const resetForm = getByTestId('reset-form');
-    fireEvent.click(resetForm);
+    const resetForm = getByTestId('reset-form')
+    fireEvent.click(resetForm)
 
     // @ts-ignore
-    expect(email.value).toBe('');
-    expect(emailError).not.toBeInTheDocument();
-    expect(passwordError).not.toBeInTheDocument();
-    expect(addressError).not.toBeInTheDocument();
-  });
+    expect(email.value).toBe('')
+    expect(emailError).not.toBeInTheDocument()
+    expect(passwordError).not.toBeInTheDocument()
+    expect(addressError).not.toBeInTheDocument()
+  })
 
   // Direct set
   it('set items values', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const email = getByTestId('email');
-    const password = getByTestId('password');
-    const setValBtn = getByTestId('set-val');
-    fireEvent.click(setValBtn);
+    const { getByTestId } = render(<BasicComponent />)
+    const email = getByTestId('email')
+    const password = getByTestId('password')
+    const setValBtn = getByTestId('set-val')
+    fireEvent.click(setValBtn)
     // @ts-ignore
-    expect(email.value).toBe('settest@test.com');
+    expect(email.value).toBe('settest@test.com')
     // @ts-ignore
-    expect(password.value).toBe('setpassword');
-  });
+    expect(password.value).toBe('setpassword')
+  })
 
   it('set items errors', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const setErrBtn = getByTestId('set-err');
-    fireEvent.click(setErrBtn);
+    const { getByTestId } = render(<BasicComponent />)
+    const setErrBtn = getByTestId('set-err')
+    fireEvent.click(setErrBtn)
 
-    const emailError = getByTestId('email-error');
-    const passwordError = getByTestId('password-error');
-    expect(emailError).toBeVisible();
-    expect(passwordError).toBeVisible();
-  });
+    const emailError = getByTestId('email-error')
+    const passwordError = getByTestId('password-error')
+    expect(emailError).toBeVisible()
+    expect(passwordError).toBeVisible()
+  })
 
   // Add or remove fields
   it('add field', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const addField = getByTestId('add');
-    fireEvent.change(addField, { target: { value: 'a' } });
-    const newField = getByTestId('new_field');
-    expect(newField).toBeVisible();
-  });
+    const { getByTestId } = render(<BasicComponent />)
+    const addField = getByTestId('add')
+    fireEvent.change(addField, { target: { value: 'a' } })
+    const newField = getByTestId('new_field')
+    expect(newField).toBeVisible()
+  })
 
   it('add multiple fields', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const addMultiple = getByTestId('add-multiple');
-    fireEvent.change(addMultiple, { target: { value: 'a' } });
-    const newFieldOne = getByTestId('new_field_1');
-    const newFieldTwo = getByTestId('new_field_2');
-    expect(newFieldOne).toBeVisible();
-    expect(newFieldTwo).toBeVisible();
-  });
+    const { getByTestId } = render(<BasicComponent />)
+    const addMultiple = getByTestId('add-multiple')
+    fireEvent.change(addMultiple, { target: { value: 'a' } })
+    const newFieldOne = getByTestId('new_field_1')
+    const newFieldTwo = getByTestId('new_field_2')
+    expect(newFieldOne).toBeVisible()
+    expect(newFieldTwo).toBeVisible()
+  })
 
   it('remove field', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const removeField = getByTestId('remove');
-    const firstName = getByTestId('first_name');
-    expect(firstName).toBeVisible();
-    fireEvent.change(removeField, { target: { value: 'a' } });
-    expect(firstName).not.toBeInTheDocument();
-  });
+    const { getByTestId } = render(<BasicComponent />)
+    const removeField = getByTestId('remove')
+    const firstName = getByTestId('first_name')
+    expect(firstName).toBeVisible()
+    fireEvent.change(removeField, { target: { value: 'a' } })
+    expect(firstName).not.toBeInTheDocument()
+  })
 
   it('remove multiple fields', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const removeFieldMultiple = getByTestId('remove_multiple');
-    const addMultiple = getByTestId('add-multiple');
-    fireEvent.change(addMultiple, { target: { value: 'a' } });
-    const newFieldOne = getByTestId('new_field_1');
-    const newFieldTwo = getByTestId('new_field_2');
-    expect(newFieldOne).toBeVisible();
-    expect(newFieldTwo).toBeVisible();
-    fireEvent.change(removeFieldMultiple, { target: { value: 'a' } });
-    expect(newFieldOne).not.toBeInTheDocument();
-    expect(newFieldTwo).not.toBeInTheDocument();
-  });
+    const { getByTestId } = render(<BasicComponent />)
+    const removeFieldMultiple = getByTestId('remove_multiple')
+    const addMultiple = getByTestId('add-multiple')
+    fireEvent.change(addMultiple, { target: { value: 'a' } })
+    const newFieldOne = getByTestId('new_field_1')
+    const newFieldTwo = getByTestId('new_field_2')
+    expect(newFieldOne).toBeVisible()
+    expect(newFieldTwo).toBeVisible()
+    fireEvent.change(removeFieldMultiple, { target: { value: 'a' } })
+    expect(newFieldOne).not.toBeInTheDocument()
+    expect(newFieldTwo).not.toBeInTheDocument()
+  })
 
   it('reset fields', () => {
-    const { getByTestId } = render(<BasicComponent />);
-    const resetField = getByTestId('reset');
-    const addField = getByTestId('add');
-    fireEvent.change(addField, { target: { value: 'a' } });
-    const newField = getByTestId('new_field');
-    expect(newField).toBeVisible();
+    const { getByTestId } = render(<BasicComponent />)
+    const resetField = getByTestId('reset')
+    const addField = getByTestId('add')
+    fireEvent.change(addField, { target: { value: 'a' } })
+    const newField = getByTestId('new_field')
+    expect(newField).toBeVisible()
 
-    fireEvent.change(resetField, { target: { value: 'a' } });
-    expect(newField).not.toBeInTheDocument();
-  });
-});
+    fireEvent.change(resetField, { target: { value: 'a' } })
+    expect(newField).not.toBeInTheDocument()
+  })
+})
